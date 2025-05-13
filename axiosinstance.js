ccb54b1b6f4eb4f1ea7dd88d3e37867205817ca3
Navpreet <navangural175@gmail.com>
2025-05-13 11:29:53 +0530

    extra

--- axiosinstance.js
+++ axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://drive-dash-backend.onrender.com",
     timeout: 5000,
 });
 
