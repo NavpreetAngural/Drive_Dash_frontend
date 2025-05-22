debd3527e48d24032eaf55c093fd8400341a4344
Navpreet <navangural175@gmail.com>
2025-05-23 00:07:33 +0530

    login and signup page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -117,7 +117,7 @@ const About = () => {
           <div className="w-[200px]">
             <img src={men_1} alt="men" />
             <center>
-              <h3> John </h3>
+              <h3 className='font-bold '> John </h3>
               <h3> Rating : 5/5 </h3>
               <span className="fa fa-star checked" />
               <span className="fa fa-star checked" />
@@ -129,7 +129,7 @@ const About = () => {
           <div className="w-[200px]">
             <img src={men_2} alt="men" />
             <center>
-              <h3> Michael </h3>
+              <h3 className='font-bold '> Michael </h3>
               <h3> Rating : 5/5 </h3>
               <span className="fa fa-star checked" />
               <span className="fa fa-star checked" />
@@ -141,7 +141,7 @@ const About = () => {
           <div className="w-[200px]">
             <img src={men_3} alt="men" />
             <center>
-              <h3> Carlos crafts </h3>
+              <h3 className='font-bold '> Carlos crafts </h3>
               <h3> Rating : 5/5 </h3>
               <span className="fa fa-star checked" />
               <span className="fa fa-star checked" />
